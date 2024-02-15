670417bdcfadb4d45190515045781d19c5db404e
Tsingper <tsingper@outlook.com>
2024-02-15 20:38:00 +0800

    fix: fix chat error

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Body } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class AIService {
@@ -8,17 +9,41 @@ export class AIService {
   async getAIResponse(message: string): Promise<string> {
     const aiApiUrl = process.env.API_URL
     const apiKey = process.env.API_KEY
-
     try {
-      const response = await this.httpService.post(
-        aiApiUrl,
-        { input: message },
-        { headers: { Authorization: `Bearer ${apiKey}` } },
-      ).toPromise();
+      const response = await lastValueFrom(
+        this.httpService.post(
+          aiApiUrl,
+          {
+            model: 'gpt-3.5-turbo',
+            messages: [
+              {
+                role: 'user',
+                content: message,
+              },
+            ],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`, // 使用你的OpenAI API密钥
+              'Content-Type': 'application/json',
+            },
+          },
+        ).pipe(
+          map(res => {
+            if (!(res.status === 200)) {
+              throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.data.choices[0].message.content;
+          }),
+        ),
+      );
 
-      return response.data.choices[0].text.trim(); // 根据AI服务的响应格式进行调整
+      return response;
     } catch (error) {
       console.error('AI Service Error:', error);
+      if(error.code === 'invalid_api_key'){
+        throw new Error('Invalid API key');
+      }
       throw new Error('Unable to get response from AI service');
     }
   }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './app.decorator';
+import { AIService } from './ai/ai.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly aiService: AIService
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('chat')
+  getChat(): Promise<string> {
+    return this.aiService.getAIResponse('你好');
   }
 }
