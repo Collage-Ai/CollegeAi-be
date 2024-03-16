af519488e9f261cf4ba7e1e5a3d3a0839f1d64c6
Tsingper <tsingper@outlook.com>
2024-03-16 22:35:00 +0800

    refactor: refactor user ask ai module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,12 +7,12 @@ import { AuthModule } from 'src/auth/auth.module';
 import { CategoryModule } from 'src/category/category.module';
 import { SmsService } from './sms.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { UserRegisteredAskAiHandler } from './askAi.service';
+// import { UserRegisteredAskAiHandler } from './askAi.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule),CategoryModule,EventEmitterModule.forRoot()],
   controllers: [UserController],
-  providers: [UserService,SmsService,UserRegisteredAskAiHandler],
+  providers: [UserService,SmsService],
   exports: [UserService, TypeOrmModule.forFeature([User])],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,7 +11,8 @@ import { CategoryService } from 'src/category/category.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { SmsService } from './sms.service';
 import { EventEmitter2 } from 'eventemitter2';
-import { UserRegisteredAskAiEvent } from './askAi.service';
+import { OnEvent } from '@nestjs/event-emitter';
+import { sendCloudFnRequest, getAIResponse, processActivityData } from 'src/util/ai';
 @Injectable()
 export class UserService {
   constructor(
@@ -133,3 +134,50 @@ export class UserService {
   }
 }
 
+
+
+// 定义事件
+export class UserRegisteredAskAiEvent {
+  constructor(
+    public readonly userPhone: string,
+    public readonly userInfo: User,
+  ) {}
+}
+
+// 定义事件处理器
+@Injectable()
+export class UserRegisteredAskAiHandler {
+  constructor(private readonly usersService: UserService) {}
+  @OnEvent('user.registered')
+  handleUserRegisteredEvent(event: UserRegisteredAskAiEvent) {
+    // 这里可以并发执行长时间运行的任务，不会阻塞用户注册
+    Promise.all([
+      sendCloudFnRequest({
+        query: event.userInfo.career,
+        isSort: false,
+        type: 'activity',
+        userInfo: JSON.stringify(event.userInfo),
+        field: event.userInfo.career,
+      }),
+      getAIResponse(JSON.stringify(event.userInfo)),
+    ])
+      .then(([skillPoint, stageAnalysis]) => {
+        const initialUser = this.usersService.findOne(event.userPhone);
+        let formatedSkillPoint = processActivityData(skillPoint)
+        let user = new UpdateUserDto();
+        user = {
+          ...initialUser,
+          skillPoint1: formatedSkillPoint[0],
+          SkillPoint2: formatedSkillPoint[1],
+          SkillPoint3: formatedSkillPoint[2],
+          stageAnalysis: stageAnalysis,
+        };
+        this.usersService.update(event.userInfo.id, user);
+      })
+      .catch((error) => {
+        // 处理错误
+        console.error('用户注册后AI分析失败', error);
+        throw new InternalServerErrorException('内部服务器错误');
+      });
+  }
+}
